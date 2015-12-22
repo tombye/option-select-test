011f20680c975bda58574df92b80f83d89f5f507
Tom Byers <tombaromba@gmail.com>
2015-12-22 14:26:17 +0000

    Set slimmer's asset host to local static server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,4 +24,6 @@ MinimumViableGovukTemplate::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.slimmer.asset_host = 'http://127.0.0.1:3013'
 end
