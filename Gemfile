22731babb98aff9c3b406e384c420e81b70f83f5
Tom Byers <tombaromba@gmail.com>
2015-12-22 14:24:54 +0000

    initial commit of minimum viable govuk template

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.2'
+
+gem 'govuk_template'
+gem 'govuk_frontend_toolkit'
+
+# Use SCSS for stylesheets
+gem 'sass-rails', '~> 4.0.0'
+
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier', '>= 1.3.0'
+
+if ENV['SLIMMER_DEV']
+  gem 'slimmer', :path => '../slimmer'
+else
+  gem 'slimmer', '9.0.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MinimumViableGovukTemplate::Application.config.secret_key_base = '56e034b550f61bb2a4e5966e4c59717964f218b639d746350d5afef03484c94fb11e4a7e662cf7acc5a6451a74e2cf0175b895eec0b17974b1d1a24750001ed3'
